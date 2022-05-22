b3a65aa6b46fb36d52060248ce73eeae36cd771b
antoninaorobets <pani.antonina.don@gmail.com>
2022-05-22 10:21:24 -0400

    commit tree adcd94fc92bc74409b3afbf7f307ca5f742f357d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,19 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do 
+    BakedGood.create(name: params[:name], price: params[:price], bakery_id: params[:bakery_id] ).to_json
+  end
+
+  patch '/bakeries/:id' do
+
+    update_bakery = Bakery.find(params[:id])
+    update_bakery.update(name: params[:name])
+    update_bakery.to_json
+  end
+  delete '/baked_goods/:id' do
+   
+    BakedGood.destroy(params[:id])
+  end
+
 end
